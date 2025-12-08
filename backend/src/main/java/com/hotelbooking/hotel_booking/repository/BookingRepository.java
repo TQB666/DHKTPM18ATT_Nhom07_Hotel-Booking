ddcc31c78ae33c812d67167ec3e4b241fb269d53
WestTand <22649051.phat@student.iuh.edu.vn>
2025-12-08 20:25:38 +0700

    done admin dashboard

--- backend/src/main/java/com/hotelbooking/hotel_booking/repository/BookingRepository.java
+++ backend/src/main/java/com/hotelbooking/hotel_booking/repository/BookingRepository.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.hotelbooking.hotel_booking.domain.Booking;
@@ -15,4 +16,9 @@ public interface BookingRepository extends JpaRepository<Booking, Long> {
 
     // thực hiện cho lịch sử đặt phòng
     List<Booking> findByUserIdOrderByIdDesc(Long userId);
+
+    @Query("SELECT SUM(b.totalPrice) FROM Booking b WHERE MONTH(b.bookingDate) = MONTH(CURRENT_DATE)")
+    Double sumMonthlyRevenue();
+
+    List<Booking> findTop10ByOrderByBookingDateDesc();
 } 
